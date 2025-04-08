f9e0e7fbade0711e163d5773bb75f6c3eeeeb0b5
HabibaMekay <habibamekay@gmail.com>
2025-04-08 20:05:23 +0200

    fixed playbar bug

--- Web/src/App.jsx
+++ Web/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
         <Route path="/" element={<Login/>}/>
         <Route path="/signup" element={<SignUp/>}/>
       </Routes>
-      {/* <Playbar/> */}
+      <Playbar/> 
     </BrowserRouter>
 
   )
