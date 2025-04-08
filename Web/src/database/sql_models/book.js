4a67372939f006ec92209e34092243c28fae6fb4
Mohaned Atef <105852138+MohanedAtef238@users.noreply.github.com>
2025-04-08 22:01:26 +0200

    picking blob approach

--- Web/src/database/sql_models/book.js
+++ Web/src/database/sql_models/book.js
@@ -7,15 +7,17 @@ const Book = sequelize.define('Book', {
   title: {type: DataTypes.STRING, allowNull: false},
   description: {type: DataTypes.TEXT, allowNull: true},
   coverImage: {type: DataTypes.STRING, allowNull: true},
+  audioFile: {type: DataTypes.BLOB('long'), allowNull: false},
+  duration: {type: DataTypes.INTEGER, allowNull: false},
   isbn: {type: DataTypes.STRING, allowNull: true, unique: true},
   publicationDate: {type: DataTypes.DATE, allowNull: true},
   genre: {type: DataTypes.STRING, allowNull: true},
   price: {type: DataTypes.DECIMAL(10, 2), allowNull: true},
   isPublished: {type: DataTypes.BOOLEAN, defaultValue: false},
   authorId: {type: DataTypes.UUID, allowNull: false, references: {model: 'Users', key: 'id'}},
-  createdAt: {type: DataTypes.DATE, defaultValue: DataTypes.NOW} 
+  createdAt: {type: DataTypes.DATE, defaultValue: DataTypes.NOW}
 }, {
-  timestamps: false 
+  timestamps: false
 });
 
 Book.belongsTo(User, { foreignKey: 'authorId', as: 'author' });
