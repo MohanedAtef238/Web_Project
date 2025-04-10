13c6138c53232000090f77c8d2cf43744684cc17
mayatarek <Mayatarek@iCloud.com>
2025-04-10 23:24:02 +0200

    added some logs for debugging

--- Web/src/database/config/database.js
+++ Web/src/database/config/database.js
@@ -6,4 +6,19 @@ const sequelize = new Sequelize('Iqraaly', 'postgres', 'postgres', {
   logging: false,
 });
 
+sequelize.authenticate()
+  .then(() => {
+    console.log('Database connected successfully!');
+  })
+  .catch((err) => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+
+  //delete this sync part after testing
+  sequelize.sync({ force: true }) // WARNING: this drops tables if they exist
+  .then(() => console.log("Tables created"))
+  .catch((err) => console.error("Error syncing DB", err));
+
+
 module.exports = sequelize; 
