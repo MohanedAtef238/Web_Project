2efff066299d327961694a706e1b5b9c74c6d4ed
Mohaned Atef <105852138+MohanedAtef238@users.noreply.github.com>
2025-04-11 12:42:45 +0200

    getting rid of duplicate server.js, adding explicity ports

--- Web/backend/config/database.js
+++ Web/backend/config/database.js
@@ -2,13 +2,14 @@ const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize('Iqraaly', 'postgres', 'postgres', {
   host: 'localhost',
+  port: 5432,
   dialect: 'postgres',
   logging: false,
 });
 
 sequelize.authenticate()
   .then(() => {
-    console.log('Database connected successfully!');
+    console.log('PostgreSQL connected successfully!');
   })
   .catch((err) => {
     console.error('Unable to connect to the database:', err);
