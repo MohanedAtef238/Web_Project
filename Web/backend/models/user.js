8f5425b190637ae976b2f017bc8210921176e95a
Mohaned Atef <105852138+MohanedAtef238@users.noreply.github.com>
2025-05-12 14:28:31 +0300

    omg

--- Web/backend/models/user.js
+++ Web/backend/models/user.js
@@ -5,6 +5,7 @@ const User = sequelize.define('User', {
   id: {type: DataTypes.UUID, defaultValue: DataTypes.UUIDV4, primaryKey: true}, // https://sequelize.org/docs/v7/models/data-types/#uuids according to this it is the best datatype for assigning primary keys and will help us when indexing in the future as it isnt incrementing. so we can apply it while making use for the trees that make up the database.
   username: {type: DataTypes.STRING, allowNull: false, unique: true},
   email: {type: DataTypes.STRING, allowNull: false, unique: true},
+  bio: {type: DataTypes.STRING, allowNull: true},
   password: {type: DataTypes.STRING, allowNull: false}, // Password validation will be added in frontend and backend
   isAuthor: {type: DataTypes.BOOLEAN, defaultValue: false}, // this was added to change the view of the profile for authors and non-authors
   createdAt: {type: DataTypes.DATE, defaultValue: DataTypes.NOW}
