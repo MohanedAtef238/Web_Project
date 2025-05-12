ef49e89eb5350f386b00d8653a5dbf67a67e4be5
Mohaned Atef <105852138+MohanedAtef238@users.noreply.github.com>
2025-05-12 13:52:15 +0300

    hard coding the api base

--- Web/src/config/api.js
+++ Web/src/config/api.js
@@ -1,2 +1,2 @@
-export const API_BASE = import.meta.env.VITE_API_BASE_URL;
-export const SIGNALING_SERVER_URL = import.meta.env.VITE_SIGNALING_SERVER_URL; 
+export const API_BASE = "backend-production-fb01.up.railway.app"
+export const SIGNALING_SERVER_URL = "backend-production-fb01.up.railway.app"
