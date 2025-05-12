09227e87dcc413992b58727152a55428787e424d
Mohaned Atef <105852138+MohanedAtef238@users.noreply.github.com>
2025-05-12 04:43:50 +0300

    smth for railway

--- Web/backend/config/database.js
+++ Web/backend/config/database.js
@@ -1,16 +1,15 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME || 'Iqraaly',
-  process.env.DB_USER || 'postgres',
-  process.env.DB_PASSWORD || 'postgres',
-  {
-    host: process.env.DB_HOST || 'localhost',
-    port: process.env.DB_PORT || 5432,
-    dialect: 'postgres',
-    logging: false,
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
   }
-);
+});
 
 sequelize.authenticate()
   .then(() => {
